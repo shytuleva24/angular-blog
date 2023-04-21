f5d4e5bfc1fcee1b16c9223b932371f60694a739
shytuleva24 <shutyleva@gmail.com>
2023-04-21 11:53:17 +0300

    first commit

---
+++ src/app/shared/posts.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {Post} from "./interfaces";
+import {environment} from "../../environments/environment";
+
+@Injectable({providedIn: "root"})
+
+export class PostService {
+
+    constructor (
+        private http: HttpClient
+    ) {}
+
+    create(post: Post): Observable<Post> {
+        return this.http.post<Post>(`${environment.fbDbUrl}/posts.json`, post)
+    }
+}
